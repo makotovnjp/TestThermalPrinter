0d92b4e3a8ef7c52ba4d3e6c6ec4dd85ae8d7637
congthanhnguyen <te304584>
2017-05-03 14:09:13 +0900

    add usb print test

---
+++ server/print/UsbPrintTest.js
@@ -0,0 +1,28 @@
+const escpos = require('escpos');
+
+// Select the adapter based on your printer type
+const device  = new escpos.USB();
+// const device  = new escpos.Network('localhost');
+// const device  = new escpos.Serial('/dev/usb/lp0');
+
+const printer = new escpos.Printer(device);
+
+device.open(function(){
+    //
+    // printer
+    //     .font('a')
+    //     .align('ct')
+    //     .style('bu')
+    //     .size(1, 1)
+    //     .text('Thanh Test')
+    //     .barcode('12345678', 'EAN8')
+    //     .qrimage('https://github.com/song940/node-escpos', function(err){
+    //         this.cut();
+    //         this.close();
+    //     });
+    //
+    printer.text("Thanh Test");
+    printer.cut();
+    printer.close();
+
+});
