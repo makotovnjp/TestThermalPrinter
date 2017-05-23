9881746c3ddbf6ebf3df589401753419e16aa65f
congthanhnguyen <te304584>
2017-05-23 14:59:44 +0900

    test

--- server/print/UsbPrintTest.js
+++ server/print/UsbPrintTest.js
@@ -1,9 +1,9 @@
 const escpos = require('escpos');
 
 // Select the adapter based on your printer type
-const device  = new escpos.USB();
+//const device  = new escpos.USB();
 // const device  = new escpos.Network('localhost');
-// const device  = new escpos.Serial('/dev/usb/lp0');
+const device  = new escpos.Serial('/dev/usb/lp0');
 
 const printer = new escpos.Printer(device);
 
